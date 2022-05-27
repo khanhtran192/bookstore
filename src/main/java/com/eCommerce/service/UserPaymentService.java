50afd805607b716a66cf763846216926feca18de
khanhtran192 <khanhtd192@gmail.com>
2022-05-27 20:23:30 +0700

    Initial commit

---
+++ src/main/java/com/eCommerce/service/UserPaymentService.java
@@ -0,0 +1,11 @@
+package com.eCommerce.service;
+
+import com.eCommerce.domain.UserPayment;
+
+public interface UserPaymentService {
+	UserPayment findById(Long id);
+	
+	void deteteById(Long id);
+
+	void deleteById(Long id);
+}
